a084cf2267cc7b9db82d80da58bd312acb2ccb73
Igor <igor.milosavljevic.1994@gmail.com>
2020-03-05 00:45:04 +0100

    comment repository get all

--- core/Repository/CommentRepository.php
+++ core/Repository/CommentRepository.php
@@ -1,13 +1,22 @@
 <?php
 
 namespace App\Repository;
+
+use App\Database\Database;
+use App\Entity\Comment;
 use PDO;
 
 
 class CommentRepository implements RepositoryInterface
 {
+    private $table = 'comments';
+
     public function get()
     {
-        // TODO: Implement get() method.
+        $connection = Database::getInstance();
+        $connection = $connection->getConnection();
+        $statement = $connection->query("SELECT * FROM $this->table");
+        $comments = $statement->fetchAll(PDO::FETCH_CLASS, Comment::class);
+        return $comments;
     }
 }
