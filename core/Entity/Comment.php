be3bb057f10da384ab57d36b0484c50999ce7d1b
Igor <igor.milosavljevic.1994@gmail.com>
2020-03-04 22:07:45 +0100

    comment and product entities

---
+++ core/Entity/Comment.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace App\Entity;
+
+
+class Comment
+{
+    private $id;
+    private $name;
+    private $email;
+    private $comment;
+    private $approved;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+    }
+
+    public function getApproved()
+    {
+        return $this->approved;
+    }
+
+    public function setApproved($approved)
+    {
+        $this->approved = $approved;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+}
---
+++ core/Entity/Product.php
@@ -0,0 +1,63 @@
+<?php
+
+
+namespace App\Entity;
+
+class Product
+{
+    private $id;
+    private $product;
+    private $description;
+    private $src;
+    private $alt;
+
+    public function getProduct()
+    {
+        return $this->product;
+    }
+
+    public function setProduct($product)
+    {
+        $this->product = $product;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getSrc()
+    {
+        return $this->src;
+    }
+
+    public function setSrc($src)
+    {
+        $this->src = $src;
+    }
+
+    public function getAlt()
+    {
+        return $this->alt;
+    }
+
+    public function setAlt($alt)
+    {
+        $this->alt = $alt;
+    }
+}
