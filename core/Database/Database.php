1f447840a0902c44970d41426c3382664e5c010e
Igor <igor.milosavljevic.1994@gmail.com>
2020-03-04 21:19:22 +0100

    refactor database class

--- core/Database/Database.php
+++ core/Database/Database.php
@@ -6,31 +6,25 @@ use PDO;
 
 class Database
 {
-    private static $instance = null;
+    private static $connection = null;
     private $host = 'localhost';
     private $dbName = 'citrus';
     private $username = 'root';
     private $password = '';
-    private $connection;
 
     private function __construct()
     {
-        $this->connection = new PDO('mysql:host=' . $this->host . ';' . 'dbname=' . $this->dbName, $this->username,
+        self::$connection = new PDO('mysql:host=' . $this->host . ';' . 'dbname=' . $this->dbName, $this->username,
         $this->password);
     }
 
-    public static function getInstance()
+    public static function getConnection()
     {
-        if(self::$instance)
+        if(self::$connection)
         {
-            return self::$instance;
+            return self::$connection;
         }
-        self::$instance = new Database();
-        return self::$instance;
-    }
-
-    public function getConnection()
-    {
-        return $this->connection;
+        self::$connection = new Database();
+        return self::$connection;
     }
 }
