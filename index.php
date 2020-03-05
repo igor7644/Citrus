c65cda8c538cff524dcf0f6309a43c604ac7346d
Igor <igor.milosavljevic.1994@gmail.com>
2020-03-05 10:59:45 +0100

    switch pages

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
 
 require 'vendor/autoload.php';
-require 'views/home.php';
+
+if(isset($_GET['page']))
+{
+    switch($_GET['page'])
+    {
+        case 'admin':
+            include 'views/admin.php';
+            break;
+        default:
+            include 'views/home.php';
+            break;
+    }
+}
+else
+{
+    include 'views/home.php';
+}
